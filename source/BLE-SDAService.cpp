1abb19dc0caa0df9dbc1d9b3532397f54c9c6445
prakharbhatt007 <prkhrbhtt@gmail.com>
2020-04-09 20:25:43 +0530

    seperate file for buffer creation acc to fragment protocol

--- source/BLE-SDAService.cpp
+++ source/BLE-SDAService.cpp
@@ -19,7 +19,7 @@ using mbed::callback;
 #define response_size 1000
 uint16_t idx = 0;
 uint8_t g_seqnum = 0;
-
+uint16_t packet = 0;
 uint16_t BLESDA::getCharacteristicHandle() {
     return sdaCharacteristic.getValueAttribute().getHandle();
 }
@@ -36,18 +36,17 @@ size_t BLESDA::write(uint8_t* buff, uint8_t length) {
 	return length;
 }
 
-sda_protocol_error_t BLESDA::BLETX(_sda_over_ble_header* header, uint8_t len){
+sda_protocol_error_t BLESDA::BLETX(Frag_buff* header, uint8_t len){
     if(ble.gap().getState().connected) {
-    uint8_t transmit_data_len = len+START_DATA_BYTE+1;
-    uint8_t* msg = (uint8_t*)malloc(transmit_data_len*sizeof(uint8_t));
-    memcpy(msg, header, START_DATA_BYTE);
-    memcpy(&msg[START_DATA_BYTE], header->payload, len);
-    write(msg, transmit_data_len);
-    free(msg);
-    return PT_ERR_OK;
+        uint8_t transmit_data_len = len+START_DATA_BYTE+1;
+        uint8_t* msg = (uint8_t*)malloc(transmit_data_len*sizeof(uint8_t));
+        memcpy(msg, header, START_DATA_BYTE);
+        memcpy(&msg[START_DATA_BYTE], header->payload, len);
+        write(msg, transmit_data_len);
+        free(msg);
+        return PT_ERR_OK;
     }
     else {
-        printf("Here");
         ble.gap().startAdvertising(ble::LEGACY_ADVERTISING_HANDLE);
         return PT_ERR_LOST_CONN;
     }
@@ -62,27 +61,20 @@ sda_protocol_error_t BLESDA::sda_fragment_datagram(uint8_t* sda_payload, uint16_
         static uint8_t frag_num=0;
         uint16_t totalpayloadsize=payloadsize;
         //Calculate number of fragment in order to be transmitted
-        uint8_t num_frag = ((payloadsize%BLE_PACKET_SIZE)==0)?(payloadsize/BLE_PACKET_SIZE):(payloadsize/BLE_PACKET_SIZE)+1;
+        uint8_t num_frag = ((payloadsize%BLE_PACKET_SIZE) == 0)?(payloadsize/BLE_PACKET_SIZE):(payloadsize/BLE_PACKET_SIZE)+1;
         while(num_frag)
         {
-            sda_over_ble_header  frag_sda ={0};
-            frag_sda.seq_num = g_seqnum;
-            frag_sda.type = type;
-            frag_sda.more_frag = (num_frag==1)?0:1;
-            frag_sda.frag_num=(++frag_num);
-            frag_sda.length=totalpayloadsize;
-            frag_sda.frag_length=(payloadsize < BLE_PACKET_SIZE)? payloadsize : BLE_PACKET_SIZE;
-            frag_sda.payload = (uint8_t*) malloc(frag_sda.frag_length);
-            if(frag_sda.payload!=NULL)
-                {
-                    memcpy(&frag_sda.payload[0],&sda_payload[fragmentStartOffset],frag_sda.frag_length);
-                }
-                else
+            uint8_t more_frag = (num_frag==1)?0:1;
+            uint16_t frag_length=(payloadsize < BLE_PACKET_SIZE)? payloadsize : BLE_PACKET_SIZE;
+
+            Frag_buff frag_sda = get_buff(g_seqnum, type, more_frag, ++frag_num, totalpayloadsize,frag_length);
+            bool success = populate_fragment_data(&frag_sda,&sda_payload[fragmentStartOffset]);
+            if(!success)
                 {
                     mbed_tracef(TRACE_LEVEL_ERROR,TRACE_GROUP_BLE,"can not create payload for BLE");
                     return PT_ERR_SEND_BLE;
                 }
-            if(payloadsize<BLE_PACKET_SIZE)
+            if(payloadsize < BLE_PACKET_SIZE)
             {
                 fragmentStartOffset = 0;
             }
@@ -93,26 +85,26 @@ sda_protocol_error_t BLESDA::sda_fragment_datagram(uint8_t* sda_payload, uint16_
             //Transmit it to BLE
             sda_protocol_error_t status = BLETX(&frag_sda,frag_sda.frag_length);
             if(status != PT_ERR_OK){
+                free(frag_sda.payload);
                 return status;
             }
             num_frag--;
             payloadsize -= frag_sda.frag_length;
-        //free allocated memory
+            //free allocated memory
             free(frag_sda.payload);
         }
         g_seqnum = g_seqnum+1;
         return PT_ERR_OK;
     }
     else{
-        printf("Here sda frag");
         ble.gap().startAdvertising(ble::LEGACY_ADVERTISING_HANDLE);
-        mbed_tracef(TRACE_LEVEL_ERROR, TRACE_GROUP_BLE, "connection lost");
+        mbed_tracef(TRACE_LEVEL_ERROR, TRACE_GROUP_BLE, "connection lost..now advertising!");
         return PT_ERR_LOST_CONN;
     }
  }
 
 
-sda_protocol_error_t BLESDA::ProcessBuffer(sda_over_ble_header* frag_sda){
+sda_protocol_error_t BLESDA::ProcessBuffer(Frag_buff* frag_sda){
     if(ble.gap().getState().connected) {
         uint8_t response[response_size]={0};
         uint16_t sda_response_size=0;
@@ -124,27 +116,24 @@ sda_protocol_error_t BLESDA::ProcessBuffer(sda_over_ble_header* frag_sda){
             return PT_ERR_MSG;
         }
         memcpy(&msg_to_sda[idx],frag_sda->payload, frag_sda->frag_length);
-        if(frag_sda->more_frag == 1){
-        idx +=frag_sda->frag_length;
-        return PT_ERR_OK;
+        if(!is_last_fragment(frag_sda)){
+            idx +=frag_sda->frag_length;
+            return PT_ERR_OK;
         }
         else{
             idx = 0;
             mbed_tracef(TRACE_LEVEL_INFO,TRACE_GROUP_BLE,"Sending buffer to SDA\r\n");
             SDAOperation sda_operation(msg_to_sda);
             sda_protocol_error_t status = sda_operation.init(response,response_size,&sda_response_size);
-            if(status !=PT_ERR_OK){
-                free(msg_to_sda);
-                msg_to_sda = NULL;
-                return status;
-            }
             free(msg_to_sda);
             msg_to_sda = NULL;
+            if(status !=PT_ERR_OK){
+                return status;
+            }
             return sda_fragment_datagram(response, sda_response_size, SDA_DATA);
         }
     }
     else{
-        printf("Process BUff");
         ble.gap().startAdvertising(ble::LEGACY_ADVERTISING_HANDLE);
         if(msg_to_sda){
             free(msg_to_sda);
@@ -154,15 +143,16 @@ sda_protocol_error_t BLESDA::ProcessBuffer(sda_over_ble_header* frag_sda){
 }
 
 void BLESDA::onDataWritten(const GattWriteCallbackParams *params) {
-    mbed_tracef(TRACE_LEVEL_INFO,TRACE_GROUP_BLE,"Data Received");
+    packet+=params->len;
+    mbed_tracef(TRACE_LEVEL_INFO,TRACE_GROUP_BLE,"Data Received %d",packet);
     if (params->handle == getCharacteristicHandle()) {
-    sda_over_ble_header buffer;
-    memcpy(&buffer, params->data, START_DATA_BYTE);
-    buffer.payload = (uint8_t*)malloc(buffer.frag_length);
-    if(buffer.payload){
-        memcpy(buffer.payload, &params->data[START_DATA_BYTE],buffer.frag_length);
-    	}
-    else {
+    Frag_buff buffer = {0};
+    bool success = populate_header(&buffer, params->data);
+    if(!success){
+        return;
+    }
+    success = populate_data(&buffer, params->data);
+    if(!success){
         return;
     }
     sda_protocol_error_t status = ProcessBuffer(&buffer);
--- source/include/BLE-SDAService.h
+++ source/include/BLE-SDAService.h
@@ -15,6 +15,7 @@
  */
 #ifndef _BLE_SDA_SERVICE_H_
 #define _BLE_SDA_SERVICE_H_
+#include "frag_buff.h"
 #include "events/EventQueue.h"
 #include "ble/UUID.h"
 #include "ble/BLE.h"
@@ -25,109 +26,56 @@
 #include "./../../ble-config-uuid.h"
 
 #define TRACE_GROUP_BLE             "BLESDA"
-#define UPPER_BYTE_REQ_INDEX    3
-#define LOWER_BYTE_REQ_INDEX    4
-#define START_DATA_BYTE         8
-#define SDA_DATA                1
-#define SDA_ACK                 3
-#define SDA_REQ                 2
-
-typedef struct _sda_over_ble_header {
-	 /* Frame sequence number(1byte) */
-	uint8_t seq_num;
-    /* control frame field 12byte) */
-	uint8_t type:2;
-    uint8_t more_frag:1;
-	uint8_t frag_num:3;
-        /* Frag length (1byte) */
-    uint8_t frag_length;
-    /* Total length (2byte) */
-    uint16_t length;
-    uint8_t resv1:2;
-	uint8_t resv2;
-    uint8_t resv3;
-    uint16_t resv4;
-
-
-	/* Payload */
-    uint8_t* payload;
-}__attribute__((packed)) sda_over_ble_header;
+#define UPPER_BYTE_REQ_INDEX            3
+#define LOWER_BYTE_REQ_INDEX            4
+#define START_DATA_BYTE                 8
+#define SDA_DATA                        1
+#define SDA_ACK                         3
+#define SDA_REQ                         2
 
 class BLESDA {
 public:
     /** Maximum length of data (in bytes) that the UART service module can transmit to the peer. */
-    static const unsigned BLE_UART_SERVICE_MAX_DATA_LEN = 244;//(BLE_GATT_MTU_SIZE_DEFAULT - 3);
+    static const unsigned BLE_UART_SERVICE_MAX_DATA_LEN = 244;
 public:
-    /**
-    * @param _ble
-    *               BLE object for the underlying controller.
-    */
     BLESDA(events::EventQueue &event_queue, BLE &_ble, const char* endpointbuffer) :
         _event_queue(event_queue),
         ble(_ble),
         _endpointBuffer(endpointbuffer),
         sdaCharacteristic(CharacteristicUUID, NULL, 1, BLE_UART_SERVICE_MAX_DATA_LEN,
-                         GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_WRITE | GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_WRITE_WITHOUT_RESPONSE
-                         |GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_NOTIFY)
+                         GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_WRITE |
+                         GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_WRITE_WITHOUT_RESPONSE |
+                         GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_NOTIFY)
         {
             GattCharacteristic *charTable[] = {&sdaCharacteristic};
             GattService SDAService(ServiceUUID, charTable, sizeof(charTable) / sizeof(GattCharacteristic *));
             ble.gattServer().addService(SDAService);
             ble.gattServer().onDataWritten(this, &BLESDA::onDataWritten);
         }
-    /**
-     * Note: TX and RX characteristics are to be interpreted from the viewpoint of the GATT client using this service.
-     */
+
     uint16_t getCharacteristicHandle();
-    /**
-     * Note: TX and RX characteristics are to be interpreted from the viewpoint of the GATT client using this service.
-     */
-    //uint16_t getCharacteristicHandle();
-    /*Need UUID of the service so that we can advertise that in the advertisement */
-    const uint8_t* getUUID(){
+
+    const uint8_t* getUUID(){   //small method that returns UUID of the service.
         return ServiceUUID;
     }
 
-    sda_protocol_error_t BLETX(sda_over_ble_header* header, uint8_t len);
-
+    sda_protocol_error_t BLETX(Frag_buff* header, uint8_t len);
     size_t write(uint8_t *_buffer, uint8_t length);
-
     int _putc(int c);
-    /**
-     * Override for Stream::_getc().
-     * @return
-     *     The character read.
-     */
     int _getc();
     uint8_t* getRecievedBuffer();
-    /**
-     * This callback allows the UART service to receive updates to the
-     * txCharacteristic. The application should forward the call to this
-     * function from the global onDataWritten() callback handler; if that's
-     * not used, this method can be used as a callback directly.
-     */
     void onDataWritten(const GattWriteCallbackParams *params);
-    /*
-    This callback allows the connecting device to fetch the endpoint of the device so that it can proceed further.
-    */
-    void onDataRead(const GattReadCallbackParams *params);
-    sda_protocol_error_t ProcessBuffer(sda_over_ble_header* frag_sda);
+    sda_protocol_error_t ProcessBuffer(Frag_buff* frag_sda);
     sda_protocol_error_t sda_fragment_datagram(uint8_t* sda_payload, uint16_t payloadsize, uint8_t type);
     sda_protocol_error_t processInputBuffer(uint8_t* msg_in, uint16_t bytesRecieved);
-    sda_protocol_error_t processRequest(sda_over_ble_header* frag_sda);
+    sda_protocol_error_t processRequest(Frag_buff* frag_sda);
 
 private:
-    //Thread t;
     char* getEndpoint();
     events::EventQueue &_event_queue;
-    BLE                 &ble;                                           //  BLe instance
-    const char*         _endpointBuffer;                                /*  The local buffer that contains the endpoint of
-                                                                        the device.       */
+    BLE                 &ble;
+    const char*         _endpointBuffer;
     uint8_t*            msg_to_sda = NULL;
-    GattCharacteristic  sdaCharacteristic;                               /**< From the point of view of the external client, this is the characteristic
-                                                                        *   they'd write into in order to communicate with this application. */
-    // GattCharacteristic  Characteristic;                               /**< From the point of view of the external client, this is the characteristic
-    //                                                                     *   they'd read from in order to receive the bytes transmitted by this
-    //                                                                     *   application. */
+    GattCharacteristic  sdaCharacteristic;
 };
 #endif
