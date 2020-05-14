3f2e1900941c74f8ad8db69ab1e0066296926e79
prakharbhatt007 <prkhrbhtt@gmail.com>
2020-05-14 17:48:18 +0530

    removed patch file generated from ubuntu from patch command and removed the patch.sh

--- patch.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-patch mbed-os/features/FEATURE_BLE/targets/TARGET_CORDIO/driver/CordioHCIDriver.cpp < patch/Patchfile1
-patch mbed-os/features/FEATURE_BLE/targets/TARGET_CORDIO/stack_adaptation/hci_tr.c < patch/Patchfile2
